4036f5a24964dff21cb30e87ce5bf801a6c3d766
MUSTAFA ÇAY <mcay51@gmail.com>
2025-05-05 21:51:01 +0300

    OOP class metod attribute constructor

--- src/main/java/tr/com/mcay/Main.java
+++ src/main/java/tr/com/mcay/Main.java
@@ -1,7 +1,59 @@
 package tr.com.mcay;
 
+import tr.com.mcay.oop.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Student student = new Student();
+        Student student2 = new Student("Zeynep","ÇAY",8,new ArrayList<>());
+        Student student3 = new Student("Küçük Mehmet Ali");
+
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        String[][] dizi=new String[4][3];
+        dizi[0][0]="Zeynep";
+        dizi[0][1]="ÇAY";
+        dizi[0][2]="8";
+        dizi[1][0]="Mehmet Ali";
+        dizi[1][1]="Bilgin";
+        dizi[1][2]="22";
+        dizi[2][0]="Burhan";
+        dizi[2][1]="Ertan";
+        dizi[2][2]="18";
+        dizi[3][0]="Mustafa";
+        dizi[3][1]="ÇAY";
+        dizi[3][2]="45";
+
+        for(int i=0;i<dizi.length;i++){
+            for(int j=0;j<dizi[i].length;j++){
+                System.out.print(dizi[i][j]+"\t");
+            }
+
+        }
+        System.out.println("\n");
+        List list = new ArrayList();
+        list.add(student);
+        list.add(student2);
+        list.add(dizi);
+        list.add("Işıl ÇAY");
+        list.add(25800);
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        List<Integer> notList=new ArrayList();
+        notList.add(80);
+        notList.add(45);
+        notList.add(70);
+       Integer ogrenciNotu = student.notHesapla(notList);
+        System.out.println( "Öğrenci Notu: "+ogrenciNotu);
+        student.notHesapla2(notList);
+
+        System.out.println("Integer Donenstudent.notHesapla(notList));
     }
+
+
 }
--- src/main/java/tr/com/mcay/oop/Student.java
+++ src/main/java/tr/com/mcay/oop/Student.java
@@ -1,4 +1,88 @@
 package tr.com.mcay.oop;
 
+import java.util.List;
+
 public class Student {
+    public String name; // class attribute  veya  instance attribute veya field
+    private String surname;
+    // double ,float,byte,short,long,char,boolean,int --> primative tipler
+    private int age;
+    private List<Integer> notlar;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", notlar=" + notlar +
+                '}';
+    }
+// private Integer age;
+
+    public Student(String name, String surname, int age, List<Integer> notlar) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.notlar = notlar;
+    }
+    public Student(String name){
+        this.name = name;
+    }
+    public Student() {
+        this.name="Mehmet Ali";
+        this.surname="Bilgin";
+        this.age=22;
+    }
+    public Integer notHesapla(List<Integer> notlar) {
+        Integer not=0;
+        for(Integer i : notlar){
+            not = not+i;
+        }
+        not=not/notlar.size();
+        System.out.println("Ortalama : "+not);
+        return not;
+    }
+    public void notHesapla2(List<Integer> notlar) {
+        Integer not=0;
+        for(Integer i : notlar){
+            not = not+i;
+        }
+        not=not/notlar.size();
+        System.out.println("Ortalama : "+not);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+/*    public Integer getInteger() {
+        return age;
+    }
+
+    public void setInteger(Integer age) {
+        this.age = age;
+    }*/
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
